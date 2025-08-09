04e812c38e2897ea1c1fb43c10ff9e1b344779b3
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2025-08-09 19:39:22 +0000

    still having problem with code deploy

--- Terraform/codeBuild.tf
+++ Terraform/codeBuild.tf
@@ -77,12 +77,13 @@ resource "aws_codebuild_project" "Terra-CodeBuild" {
   service_role  = aws_iam_role.Terra-Role.arn
 
   artifacts {
-    type      = "S3"
-    location  = aws_s3_bucket.Terra-S3.bucket
-    packaging = "ZIP"
-    path      = "/"
-    name      = "build-output"
-  }
+  type      = "S3"
+  location  = aws_s3_bucket.Terra-S3.bucket
+  packaging = "ZIP"
+  path      = "/"
+  name      = "build-output"
+}
+
 
 
   cache {
