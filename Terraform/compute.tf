bac325fe547b05d939de605dc050c883e10d4f7c
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2025-08-09 18:07:43 +0000

    finished code deploy

--- Terraform/compute.tf
+++ Terraform/compute.tf
@@ -18,8 +18,20 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y ruby wget
+              cd /home/ubuntu
+              wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+              chmod +x ./install
+              ./install auto
+              systemctl start codedeploy-agent
+              systemctl enable codedeploy-agent
+              EOF
+
   tags = {
     Name = "EC2-CODE-DEPLOY"
-    Env = "Production"
+    Env  = "Production"
   }
 }
