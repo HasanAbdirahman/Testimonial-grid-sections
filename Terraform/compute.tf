ee29453cbb2455bc08fe5a96a76cd182032c9a04
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2025-08-11 19:56:16 +0000

    done

--- Terraform/compute.tf
+++ Terraform/compute.tf
@@ -1,58 +1,3 @@
-# IAM Role for EC2 instance to allow CodeDeploy access
-resource "aws_iam_role" "ec2_codedeploy_role" {
-  name = "EC2CodeDeployRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-# Custom IAM policy for CodeDeploy EC2 permissions
-resource "aws_iam_policy" "codedeploy_ec2_policy" {
-  name        = "CodeDeployEC2Policy"
-  description = "Policy for EC2 instances to work with CodeDeploy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "codedeploy:*",
-          "s3:Get*",
-          "s3:List*",
-          "ec2:Describe*",
-          "tag:GetTags",
-          "cloudwatch:PutMetricData",
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Attach custom policy to the EC2 role
-resource "aws_iam_role_policy_attachment" "ec2_codedeploy_policy" {
-  role       = aws_iam_role.ec2_codedeploy_role.name
-  policy_arn = aws_iam_policy.codedeploy_ec2_policy.arn
-}
-
-# Create IAM instance profile for the EC2 role
-resource "aws_iam_instance_profile" "ec2_codedeploy_profile" {
-  name = "EC2CodeDeployInstanceProfile"
-  role = aws_iam_role.ec2_codedeploy_role.name
-}
-
 # Data source to get latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -69,12 +14,40 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+# EC2 instance role trusted by EC2 service
+resource "aws_iam_role" "ec2_codedeploy_role" {
+  name = "EC2CodeDeployRole"
 
-# EC2 instance with CodeDeploy agent installation
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Attach AWS managed policy for CodeDeploy to EC2 role
+resource "aws_iam_role_policy_attachment" "ec2_managed_policy" {
+  role       = aws_iam_role.ec2_codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+}
+
+# Instance profile for EC2 role
+resource "aws_iam_instance_profile" "ec2_codedeploy_profile" {
+  name = "EC2CodeDeployInstanceProfileV2"  # changed here
+  role = aws_iam_role.ec2_codedeploy_role.name
+}
+
+
+# Example EC2 instance (add iam_instance_profile)
 resource "aws_instance" "web" {
-  ami                  = data.aws_ami.ubuntu.id
-  instance_type        = "t3.micro"
-  iam_instance_profile = aws_iam_instance_profile.ec2_codedeploy_profile.name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_codedeploy_profile.name
 
   user_data = <<-EOF
     #!/bin/bash
